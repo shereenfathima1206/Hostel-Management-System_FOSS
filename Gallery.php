107aa47177bd144e5fd3a95492963fae58fe8c98
shereenfathima1206 <shereenfathima1206@gmail.com>
2017-04-26 22:09:57 +0530

    Create Gallery Module

---
+++ Gallery.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+<meta charset="utf-8" />
+<meta name="author" content="Script Tutorials" />
+<title>gallery</title>
+<link href="gallery.css" rel="stylesheet" type="text/css" />
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+<h1><i>gallery</i></h1>
+<div class="container" id="container">
+<div class="gallery">
+<a tabindex="1"><imgsrc="1.jpg"></a>
+<a tabindex="1"><imgsrc="2.jpg"></a>
+<a tabindex="1"><imgsrc="3.jpg"></a>
+<a tabindex="1"><imgsrc="4.jpg"></a>
+<a tabindex="1"><imgsrc="5.jpg"></a>
+<a tabindex="1"><imgsrc="6.jpg"></a>
+<a tabindex="1"><imgsrc="7.jpg"></a>
+<a tabindex="1"><imgsrc="8.jpg"></a>
+<span class="close"></span>
+</div>
+</div>
+<script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="https://www.script-tutorials.com/assets/ads.js" async></script>
+</body>
+</html>
