dde5bd898711ec20e0c17462cf87643b4f549302
shereenfathima1206 <shereenfathima1206@gmail.com>
2017-04-26 22:10:41 +0530

    Create Feedback Module

---
+++ Feedback.php
@@ -0,0 +1,20 @@
+<?php
+//feedback
+?>
+<html>
+<head>
+<meta charset="utf-8">
+<title>feedback/complaints</title>
+</head>
+<body background="bg6.jpg">
+	<imgsrc="fb.jpg" height="300px" />
+<div id="feedback">
+<h1><strong></strong><u>Please enter your feedback , suggestions and complaints.</u></h1>
+<form action="insert.php" method="post" name="fb">
+<p style="font-size:40px"> name <input type="text" name="name" style="font-size:30px"/></p>
+<p style="font-size:40px">feedback/suggestions/complaints<input type="text" name="feedback" style="height:200px;width:1000px;font-size:30px"/></p>
+<p><input type="submit" onclick=" value="submit style="font-size:40px""/></p>
+</form>
+</div><!-- end login -->
+</body>
+</html>
